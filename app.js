89cd5709a141df6dd93fb2a98bfd44b3d33558f5
FerayKizilsu <semihkizilsu@Semihs-MacBook-Pro.local>
2019-07-01 18:04:38 +0300

    routing with express.js

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-const http = require('http');
-const routes = require('./routes');
+const path = require('path');
 
-const server = http.createServer(routes.handler); //event driven architecture
+const express = require('express');
+const bodyParser = require('body-parser');
 
-server.listen(3002);
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) =>{
+    res.status(404).render('404',{pageTitle:'Page Not Found'});
+    //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});//any routing method this method call and response 404 status
+
+app.listen(3002); //event driven architecture
+
